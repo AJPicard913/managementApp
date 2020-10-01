5d2100eb1503e59309fbe817b16511e37d77af84
ajpicard913 <Contact@saucedesigns.io>
2020-10-01 18:58:01 -0400

    Initial Commit

---
+++ Shared/ManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagementAppApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 10/1/20.
+//
+
+import SwiftUI
+
+@main
+struct ManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
